3df2c0d3d1f48d0ff76d9548adca857bad5cc5d6
KNKV <kunakov@mail.ru>
2014-05-29 16:25:37 +0400

    test

--- src/test/java/critical/authorized/Authorized.java
+++ src/test/java/critical/authorized/Authorized.java
@@ -1,50 +1,68 @@
 package critical.authorized;
 
 
+import model.Captcha;
 import model.DriverFactory;
+import model.Environment;
 import model.Utils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Authorized {
 
+    private String baseUrl;
+    private String loginMerchant;
+    private String passwordMerchant;
 
-    private String baseUrl = "https://secure.payonlinesystem.com/";
-    private String loginMerchant = "info@smart-travel.ru";
-    private String passwordMerchant = "tester123";
-    private String captcha = "ability";
+    @BeforeTest
+    public void setUp(){
+
+        String [] parameters = Environment.readFile();
+        baseUrl = parameters[0];
+        loginMerchant = parameters[3];
+        passwordMerchant = parameters[4];
+    }
 
 
-    @Test (dataProviderClass = model.DataProviders.class, dataProvider = "listFailLogin")
+    @Test (dataProviderClass = model.DataProviders.class, dataProvider = "listFailLogin", enabled = true)
     public void failLogin(String failLogin){
 
         WebDriver driver = DriverFactory.getInstance().getDriver();
+        long id = System.currentTimeMillis();
+
         //authorized
         driver.get(baseUrl + "login/");
-        Utils.authorized(driver, failLogin, passwordMerchant, captcha);
+        Utils.authorized(driver, failLogin, passwordMerchant, Captcha.getCaptcha(driver));
 
         //check error message
         Assert.assertTrue(driver.findElement(By.className("warn")).isDisplayed(), "Error message is not displayed!");
     }
 
-    @Test (dataProviderClass = model.DataProviders.class, dataProvider = "listFailLoginAndPassword")
+    @Test (dataProviderClass = model.DataProviders.class, dataProvider = "listFailLoginAndPassword", enabled = true)
     public void failLoginAndPassword(String failLogin, String failPassword){
 
         WebDriver driver = DriverFactory.getInstance().getDriver();
+        long id = System.currentTimeMillis();
+
         //authorized
         driver.get(baseUrl + "login/");
-        Utils.authorized(driver, failLogin, failPassword, captcha);
+        Utils.authorized(driver, failLogin, failPassword, Captcha.getCaptcha(driver));
 
         //check error message
         Assert.assertTrue(driver.findElement(By.className("warn")).isDisplayed(), "Error message is not displayed!");
     }
 
-    @Test (dataProviderClass = model.DataProviders.class, dataProvider = "listFailCaptcha")
+    @Test (dataProviderClass = model.DataProviders.class, dataProvider = "listFailCaptcha", enabled = true)
     public void failCaptcha(String failCaptcha){
 
         WebDriver driver = DriverFactory.getInstance().getDriver();
+        long id = System.currentTimeMillis();
+
         //authorized
         driver.get(baseUrl + "login/");
         Utils.authorized(driver, loginMerchant, passwordMerchant, failCaptcha);
@@ -52,9 +70,4 @@ public class Authorized {
         //check error message
         Assert.assertTrue(driver.findElement(By.className("warn")).isDisplayed(), "Error message is not displayed!");
     }
-
-   /* @AfterClass
-    public void finish(){
-        driver.quit();
-    }*/
 }
